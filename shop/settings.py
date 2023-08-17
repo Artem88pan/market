510daced6583d79c5af7f4a8fde03b86967a7221
artem <vonap88@mail.ru>
2023-08-17 19:34:36 +0300

    add

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop-int',
+    'cart',
+    'orders',
+    'users',
+    'products'
 ]
 
 MIDDLEWARE = [
